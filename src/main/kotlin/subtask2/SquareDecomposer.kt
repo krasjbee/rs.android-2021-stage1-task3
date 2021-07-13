02486107030d8a7d3ab33e7cb8fd16e2e1d0525f
krasjbee <zloyjb@gmail.com>
2021-07-13 09:21:03 +0700

    feat:task 2 finished

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,27 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return factor(number * number, number - 1)
+    }
+
+    private fun factor(number: Int, potentialNumber: Int): Array<Int>? {
+        for (i in potentialNumber downTo 1) {
+            val rem = number - i * i
+            //recursion stop conditions
+            if (rem == 0) return arrayOf(i)
+            if (rem < 0) return null
+
+            var nextCandidate = sqrt(rem.toDouble()).toInt()
+            if (nextCandidate >= i) {
+                nextCandidate = i - 1
+            }
+            val nextPotential = factor(rem, nextCandidate)
+            if (nextPotential != null) return nextPotential + arrayOf(i)
+        }
+        return null
     }
 }
